c4262fb56e567759cd6ca37ba656bbe912adb18a
Kasun Amarasinghe <kasun.ac.m1@gmail.com>
2018-08-04 17:44:10 +1000

    Capitalized the first letter of the Library class name

--- src/FixBookControl.java
+++ src/FixBookControl.java
@@ -4,12 +4,12 @@ public class FixBookControl {
 	private enum CONTROL_STATE { INITIALISED, READY, FIXING };
 	private CONTROL_STATE state;
 	
-	private library library;
+	private Library library;
 	private book currentBook;
 
 
 	public FixBookControl() {
-		this.library = library.INSTANCE();
+		this.library = Library.INSTANCE();
 		state = CONTROL_STATE.INITIALISED;
 	}
 	
