a758c464be1bd3582d0e871e63456ffcd5ed4fff
tgcsu515 <41317775+tgcsu515@users.noreply.github.com>
2018-08-11 17:24:28 +1000

    Changed cal variable name to currentCalendar

--- src/Calendar.java
+++ src/Calendar.java
@@ -4,11 +4,11 @@ import java.util.concurrent.TimeUnit;
 public class Calendar {
 	
 	private static Calendar self;
-	private static java.util.Calendar cal;
+	private static java.util.Calendar currentCalendar; 				//Author: Kanchan Bala, I have changed the variable name from cal to currentCalendar
 	
 	
 	private Calendar() {
-		cal = java.util.Calendar.getInstance();
+		currentCalendar = java.util.Calendar.getInstance();
 	}
 	
 	public static Calendar getInstance() {
@@ -19,21 +19,22 @@ public class Calendar {
 	}
 	
 	public void incrementDate(int days) {
-		cal.add(java.util.Calendar.DATE, days);		
+		currentCalendar.add(java.util.Calendar.DATE, days);		
 	}
 	
 	public synchronized void setDate(Date date) {
 		try {
-			cal.setTime(date);
-	        cal.set(java.util.Calendar.HOUR_OF_DAY, 0);  
-	        cal.set(java.util.Calendar.MINUTE, 0);  
-	        cal.set(java.util.Calendar.SECOND, 0);  
-	        cal.set(java.util.Calendar.MILLISECOND, 0);
+			currentCalendar.setTime(date);
+	        currentCalendar.set(java.util.Calendar.HOUR_OF_DAY, 0);  
+	        currentCalendar.set(java.util.Calendar.MINUTE, 0);  
+	        currentCalendar.set(java.util.Calendar.SECOND, 0);  
+	        currentCalendar.set(java.util.Calendar.MILLISECOND, 0);
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
 		}	
 	}
+	
 	public synchronized Date Date() {
 		try {
 	        cal.set(java.util.Calendar.HOUR_OF_DAY, 0);  
