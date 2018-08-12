754a6b863344dd210831defdce0daf0793791e2c
Kasun Amarasinghe <kasun.ac.m1@gmail.com>
2018-08-12 14:48:22 +1000

    Added comments to the updates variables which were not meaningful in FixBookControl class

--- src/FixBookControl.java
+++ src/FixBookControl.java
@@ -2,7 +2,7 @@ public class FixBookControl {
 	
 	private FixBookUI currentFixBookUI;
 	private enum CONTROL_STATE { INITIALISED, READY, FIXING };
-	private CONTROL_STATE currentState; 
+	private CONTROL_STATE currentState; //Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe
 	
 	private Library library; //Capitalized the first letter of the Library class name - Author: Kasun Amarasinghe
 	private Book currentBook; //Capitalized the first letter of the Book class - Author: Kasun Amarasinghe
@@ -10,21 +10,23 @@ public class FixBookControl {
 
 	public FixBookControl() {
 		this.library = Library.INSTANCE();//Capitalized the first letter of the Library class name - Author: Kasun Amarasinghe
-		currentState = CONTROL_STATE.INITIALISED;
+		currentState = CONTROL_STATE.INITIALISED; //Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe
 	}
 	
 	
 	public void setUI(FixBookUI ui) {
-		if (!currentState.equals(CONTROL_STATE.INITIALISED)) {
+		//Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe
+		if (!currentState.equals(CONTROL_STATE.INITIALISED)) { 
 			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.currentFixBookUI = ui;
 		ui.setState(FixBookUI.UI_STATE.READY);
-		currentState = CONTROL_STATE.READY;		
+		currentState = CONTROL_STATE.READY;	//Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe	
 	}
 
 
 	public void bookScanned(int bookId) {
+		//Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe
 		if (!currentState.equals(CONTROL_STATE.READY)) {
 			throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
 		}	
@@ -39,11 +41,12 @@ public class FixBookControl {
 		}
 		currentFixBookUI.display(currentBook.toString());
 		currentFixBookUI.setState(FixBookUI.UI_STATE.FIXING);
-		currentState = CONTROL_STATE.FIXING;		
+		currentState = CONTROL_STATE.FIXING; //Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe		
 	}
 
 
 	public void fixBook(boolean fix) {
+		//Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe
 		if (!currentState.equals(CONTROL_STATE.FIXING)) {
 			throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
 		}	
@@ -52,11 +55,12 @@ public class FixBookControl {
 		}
 		currentBook = null;
 		currentFixBookUI.setState(FixBookUI.UI_STATE.READY);
-		currentState = CONTROL_STATE.READY;		
+		currentState = CONTROL_STATE.READY;	 //Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe	
 	}
 
 	
 	public void scanningComplete() {
+		//Updated the variable name "state" to "currentState" - Author: Kasun Amarasinghe
 		if (!currentState.equals(CONTROL_STATE.READY)) {
 			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
 		}	
