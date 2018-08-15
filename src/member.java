ffe021a360a7a24ef823169a4131e75e57b4d314
tgcsu515 <41317775+tgcsu515@users.noreply.github.com>
2018-08-15 20:07:37 +0530

    Update member.java
    Correct the method and variable name as according to the provided code style guidelines

--- src/member.java
+++ src/member.java
@@ -5,48 +5,49 @@ import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("serial")
-public class member implements Serializable {
-
-	private String LN;
-	private String FN;
-	private String EM;
-	private int PN;
-	private int ID;
-	private double FINES;
+public class Member implements Serializable {
+//change class name member to 'Member'
+	private String lastName; //Change variable name LN to 'lastName' - Author: arashdeep kaur
+	private String firstName; //change variable name FN to 'Firstname' - Author: arashdeep kaur
+	private String email; //change variable name EM to 'email' - Author: arashdeep kaur
+	private int phoneNo; //change variable name PN to 'phoneNo' - Author: arashdeep kaur
+	private int id;  //change object name ID to 'id' - Author: arashdeep kaur
+	private double fines; //change variable name FINES to 'fines' - Author: arashdeep kaur
 	
 	private Map<Integer, loan> LNS;
 
 	
 	public member(String lastName, String firstName, String email, int phoneNo, int id) {
-		this.LN = lastName;
-		this.FN = firstName;
-		this.EM = email;
-		this.PN = phoneNo;
-		this.ID = id;
+		this.lastName = lastName; //change object name LN to 'lastName' - Author: arashdeep kaur
+		this.firstName = firstName; //change object name FN to 'firstName' - Author: arashdeep kaur
+		this.email = email;  //change object name EM to 'email' - Author: arashdeep kaur
+		this.phoneNo = phoneNo;  //change object name PN to 'phoneNo' - Author: arashdeep kaur
+		this.id = id;  //change object name ID to 'id' - Author: arashdeep kaur
 		
 		this.LNS = new HashMap<>();
 	}
 
 	
 	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Member:  ").append(ID).append("\n")
-		  .append("  Name:  ").append(LN).append(", ").append(FN).append("\n")
-		  .append("  Email: ").append(EM).append("\n")
-		  .append("  Phone: ").append(PN)
+		StringBuilder stringBuilder = new StringBuilder(); //change the object name sb to 'stringzbuilder' - Author : Arashdeep kaur
+		stringBuilder.append("Member:  ").append(id).append("\n") //change the object name ID to 'id' - Author : Arashdeep kaur
+		  .append("  Name:  ").append(lastName).append(", ").append(firstName).append("\n") //change the object name LN to 'lastName' and FN to 'firstName'- Author : Arashdeep kaur
+		  .append("  Email: ").append(email).append("\n") //change the object name EM to 'email' - Author : Arashdeep kaur
+		  .append("  Phone: ").append(phoneNo)  //change the object name PN to 'phoneNo' - Author : Arashdeep kaur
 		  .append("\n")
-		  .append(String.format("  Fines Owed :  $%.2f", FINES))
-		  .append("\n");
+		  .append(String.format("  Fines Owed :  $%.2f", fines)) //change the object name FINES to 'fines' - Author : Arashdeep kaur
+		  .append("\n");   //Change the object name sb to 'stringBuilder' - Author : Arashdeep Kaur
 		
-		for (loan loan : LNS.values()) {
-			sb.append(loan).append("\n");
+		for (Loan loan : LNS.values())   //change the name loan to 'Loan' - Author : arashdeep kaur
+		{
+			stringBuilder.append(loan).append("\n"); //Change the object name sb to 'stringBuilder' - Author : Arashdeep Kaur
 		}		  
-		return sb.toString();
+		return stringBuilder.toString();  //Change the object name sb to 'stringBuilder' - Author : Arashdeep Kaur
 	}
 
 	
 	public int getId() {
-		return ID;
+		return id; // change the object name ID to id- Author Arashdeep kaur
 	}
 
 	
@@ -61,11 +62,12 @@ public class member implements Serializable {
 
 	
 	public double getFinesOwed() {
-		return FINES;
+		return fines; //change name FINES to fines - Author: arashdeep kaur
 	}
 
 	
-	public void takeOutLoan(loan loan) {
+	public void takeOutLoan(Loan loan)  //change the name loan to 'Loan' - Author : arashdeep kaur
+	{
 		if (!LNS.containsKey(loan.getId())) {
 			LNS.put(loan.getId(), loan);
 		}
@@ -76,17 +78,17 @@ public class member implements Serializable {
 
 	
 	public String getLastName() {
-		return LN;
+		return lastName; //change variable name LN to 'lastName' - Author: arashdeep kaur
 	}
 
 	
 	public String getFirstName() {
-		return FN;
+		return firstName; //change variable name FN to 'firstName' -Auther: arashdeep kaur
 	}
 
 
 	public void addFine(double fine) {
-		FINES += fine;
+		fines += fine;  //change variable name FINES to 'fines' - Author: arashdeep kaur
 	}
 	
 	public double payFine(double amount) {
@@ -94,18 +96,20 @@ public class member implements Serializable {
 			throw new RuntimeException("Member.payFine: amount must be positive");
 		}
 		double change = 0;
-		if (amount > FINES) {
-			change = amount - FINES;
-			FINES = 0;
+		if (amount > fines) {
+			change = amount - fines;
+			fines = 0;
+			//change variable name FINES to fines - Author: arashdeep kaur
 		}
 		else {
-			FINES -= amount;
+			fines -= amount;  //change variable name FINES to fines - Author: arashdeep kaur
 		}
 		return change;
 	}
 
 
-	public void dischargeLoan(loan loan) {
+	public void dischargeLoan(Loan loan) //change the name loan to 'Loan' - Author : arashdeep kaur
+	{
 		if (LNS.containsKey(loan.getId())) {
 			LNS.remove(loan.getId());
 		}
