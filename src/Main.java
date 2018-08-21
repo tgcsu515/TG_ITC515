15e2b078fde0f3362ef919cfd32450618259a718
Kasun Amarasinghe <kasun.ac.m1@gmail.com>
2018-08-21 18:32:11 +1000

    Updated the inline comments of Main.java after reviewing

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Scanner input; //Changed the varible name "IN" to "input" to be meaningful-Author Kasun Amarasinghe 
-    private static Library libraryObj; //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
-    private static String mainMenu; //Updated the varible name "MENU" to "mainMenu"-Author Kasun Amarasinghe
-    private static Calendar calendarObj; //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author Kasun Amarasinghe
-    private static SimpleDateFormat dateFormat; //Changed the varible name "SDF" to "dateFormat"-Author Kasun Amarasinghe 
+    private static Scanner input; //Changed the varible name "IN" to "input" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala 
+    private static Library libraryObj; //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+    private static String mainMenu; //Updated the varible name "MENU" to "mainMenu"-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+    private static Calendar calendarObj; //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+    private static SimpleDateFormat dateFormat; //Changed the varible name "SDF" to "dateFormat"-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala 
 
-    private static String get_menu() {// Chaged the first letter to lowercase in the method name-Author Kasun Amarasinghe
+    private static String get_menu() {// Chaged the first letter to lowercase in the method name-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
         StringBuilder stringBuilderObj = new StringBuilder();
 
         stringBuilderObj.append("\nLibrary Main Menu\n\n")
@@ -42,30 +42,30 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            input = new Scanner(System.in); //Changed the varible name "IN" to "input" to be meaningful-Author Kasun Amarasinghe
-            libraryObj = Library.INSTANCE(); //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
-            calendarObj = Calendar.getInstance(); //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author Kasun Amarasinghe
-            dateFormat = new SimpleDateFormat("dd/MM/yyyy"); //Changed the varible name "SDF" to "dateFormat"-Author Kasun Amarasinghe 
+            input = new Scanner(System.in); //Changed the varible name "IN" to "input" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+            libraryObj = Library.INSTANCE(); //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+            calendarObj = Calendar.getInstance(); //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+            dateFormat = new SimpleDateFormat("dd/MM/yyyy"); //Changed the varible name "SDF" to "dateFormat"-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala 
 
-            //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
-            for (Member currentMember : libraryObj.Members()) {// Chaged the first letter to uppercase in Member class name-Author Kasun Amarasinghe
+            //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+            for (Member currentMember : libraryObj.Members()) {// Chaged the first letter to uppercase in Member class name-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
                 output(m);
             }
             output(" ");
             //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
-            for (Book currentBook : libraryObj.Books()) { // Chaged the first letter to uppercase in Book class name-Author Kasun Amarasinghe
+            for (Book currentBook : libraryObj.Books()) { // Chaged the first letter to uppercase in Book class name-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
                 output(b);
             }
 
-			//Updated the varible name "MENU" to "mainMenu"-Author Kasun Amarasinghe
-            mainMenu = get_menu();// Chaged the first letter to lowercase in the method name-Author Kasun Amarasinghe
+			//Updated the varible name "MENU" to "mainMenu"-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+            mainMenu = get_menu();// Chaged the first letter to lowercase in the method name-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
 
             boolean e = false;
 
             while (!e) {
-                //Changed the varible name "SDF" to "dateFormat"-Author Kasun Amarasinghe 
-                output("\n" + dateFormat.format(calendarObj.Date())); //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author Kasun Amarasinghe
-                String c = input(mainMenu); //Updated the varible name "MENU" to "mainMenu"-Author Kasun Amarasinghe
+                //Changed the varible name "SDF" to "dateFormat"-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala 
+                output("\n" + dateFormat.format(calendarObj.Date())); //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+                String c = input(mainMenu); //Updated the varible name "MENU" to "mainMenu"-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
 
                 switch (c.toUpperCase()) {
 
@@ -132,24 +132,24 @@ public class Main {
 
     private static void listCurrentLoans() {
         output("");
-		// Chaged the first letter to uppercase in Loan class name-Author Kasun Amarasinghe
-        for (Loan loan : libraryObj.CurrentLoans()) { //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
+		// Chaged the first letter to uppercase in Loan class name-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+        for (Loan loan : libraryObj.CurrentLoans()) { //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
             output(loan + "\n");
         }
     }
 
     private static void listBooks() {
         output("");
-		// Chaged the first letter to uppercase in Book class name-Author Kasun Amarasinghe
-        for (Book book : libraryObj.Books()) { //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
+		// Chaged the first letter to uppercase in Book class name-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+        for (Book book : libraryObj.Books()) { //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
             output(book + "\n");
         }
     }
 
     private static void listMembers() {
         output("");
-		// Chaged the first letter to uppercase in Member class name-Author Kasun Amarasinghe
-        for (Member member : libraryObj.Members()) { //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
+		// Chaged the first letter to uppercase in Member class name-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+        for (Member member : libraryObj.Members()) { //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
             output(member + "\n");
         }
     }
@@ -169,10 +169,10 @@ public class Main {
     private static void incrementDate() {
         try {
             int days = Integer.valueOf(input("Enter number of days: ")).intValue();
-            calendarObj.incrementDate(days); //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author Kasun Amarasinghe
-            libraryObj.checkCurrentLoans(); //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
+            calendarObj.incrementDate(days); //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+            libraryObj.checkCurrentLoans(); //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
             //Changed the varible name "SDF" to "dateFormat"-Author Kasun Amarasinghe 
-            output(dateFormat.format(calendarObj.Date())); //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author Kasun Amarasinghe
+            output(dateFormat.format(calendarObj.Date())); //Renamed the varible name "CAL" to "calendarObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
 
         } catch (NumberFormatException e) {
             output("\nInvalid number of days\n");
@@ -184,8 +184,8 @@ public class Main {
         String author = input("Enter author: ");
         String title = input("Enter title: ");
         String callNo = input("Enter call number: ");
-		// Chaged the first letter to uppercase in Book class name-Author Kasun Amarasinghe
-        Book book = libraryObj.Add_book(author, title, callNo); //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
+		// Chaged the first letter to uppercase in Book class name-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
+        Book book = libraryObj.Add_book(author, title, callNo); //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
         output("\n" + book + "\n");
 
     }
@@ -197,7 +197,7 @@ public class Main {
             String email = input("Enter email: ");
             int phoneNo = Integer.valueOf(input("Enter phone number: ")).intValue();
 			// Chaged the first letter to uppercase in Member class name-Author Kasun Amarasinghe
-            Member member = libraryObj.Add_mem(lastName, firstName, email, phoneNo); //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author Kasun Amarasinghe
+            Member member = libraryObj.Add_mem(lastName, firstName, email, phoneNo); //Renamed the varible name "LIB" to "libraryObj" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
             output("\n" + member + "\n");
 
         } catch (NumberFormatException e) {
@@ -207,7 +207,7 @@ public class Main {
 
     private static String input(String prompt) {
         System.out.print(prompt);
-        return input.nextLine(); //Changed the varible name "IN" to "input" to be meaningful-Author Kasun Amarasinghe
+        return input.nextLine(); //Changed the varible name "IN" to "input" to be meaningful-Author: Kasun Amarasinghe/Reviewed By Kanchan Bala
     }
 
     private static void output(Object object) {
